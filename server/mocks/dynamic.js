61f212a4090c052e80be1ace52f767bdcc0f1487
Travis Hoover <thoov7@gmail.com>
2016-11-26 11:20:25 -0800

    deploy

--- server/mocks/dynamic.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-  var intervalFunction;
-  var WebSocketServer = require('ws').Server;
-  var socketServer = new WebSocketServer({port: 8084});
-
-  socketServer.on('connection', function(ws) {
-    console.log('Someone has connected. ' + ws.upgradeReq.url);
-  });
-};
--- tests/dummy/app/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  actions: {
-    onopen: function() {
-      this.send('emit', 'hello world');
-    }
-  }
-});
